2a8c8c8a175e82821a4efa12f79235f2ca13d9bc
Karen Astell <KarenAstell@gmail.com>
2020-06-03 17:06:09 -0400

    added classes to employee and required it to the intern, manager, and engineer class pages which also now have classes and placeholders for functions

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee{
+    constructor(github){
+     super(name, id, email); 
+     // github username 
+     this.github = github; 
+    }
+    getGithub(){}
+    // overridden to return "Engineer"
+    getRole(){}
+
+}
