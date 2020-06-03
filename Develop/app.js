3a6b7b619d2ce0203f781ed332e89494693b9225
Karen Astell <KarenAstell@gmail.com>
2020-06-03 18:18:27 -0400

    added arrays for each team member inquirer questions

--- Develop/app.js
+++ Develop/app.js
@@ -10,42 +10,94 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// what will be used to display data after inquirer is done and everything is pushed into the array
+fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+
+// is populated with information from the prompts about each team memeber
+const teamMembers = [];
+
+// makes the HTML with the teamMember info from the teamMember array
+render(teamMembers);
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const managerInfo = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter manager's name: ",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your manager's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your manager's email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your manager's office number?",
+  },
+];
+
+const addMembers = [
+  {
+    type: "list",
+    name: "role",
+    message: "Who do you want to add next?",
+    choices: ["Engineer", "Intern", "None"],
+  },
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter engineer's name: ",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your engineer's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your engineer's email?",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "What is your engineer's gitHub username?",
+  },
+];
+
+const intern = [
     {
         type: "input",
         name: "name",
-        message: "Enter MANAGER'S name: "
-    },
-    {
+        message: "Enter intern's name: ",
+      },
+      {
         type: "input",
         name: "id",
-        message: "What is your manager's ID?"
-    },
-    {
+        message: "What is your intern's ID?",
+      },
+      {
         type: "input",
         name: "email",
-        message: "What is your manager's EMAIL?"
-    }
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your manager's OFFICE NUMBER?"
-    }
+        message: "What is your intern's email?",
+      },
+      {
+          type: "input",
+          name: "school",
+          message: "Where does your inter go to school?"
+      }
 ]
-
-const addMembers = [
-    {
-        type: "list",
-        name: "role",
-        message: "Who do you want to add next?",
-        choices: ["Engineer", "Intern", "None"]
-    }
-]
-
-
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
